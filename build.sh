5483af2a02fa02e4ba4ab606eb5f0067e1985f2b
nie.chao <nie.chao@advantech.com.cn>
2024-07-23 10:58:32 +0800

    https

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t my_zhi_hu_web .
-docker create --name my_zhi_hu_web -p 80:80 my_zhi_hu_web
-docker start --restart=always my_zhi_hu_web
+docker create --name my_zhi_hu_web -p 80:80 -p 443:443 my_zhi_hu_web
+docker start my_zhi_hu_web
